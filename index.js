042b2478ef8095dd247ca5ea9421f1fd2e11af94
Paulo Freitas <paulo.freitas.nt@gmail.com>
2022-10-26 14:26:41 -0300

    Definindo rotas

--- index.js
+++ index.js
@@ -11,11 +11,49 @@
 // testar();
 
 //Sincronizar o banco de dados
-const pessoa = require('./models/Pessoa');
+// const pessoa = require('./models/Pessoa');
+// async function sincronizar(){
+//   await pessoa.sync();
+//   console.log('Sincronizado');
+// }
+// sincronizar();
 
-async function sincronizar(){
-  await pessoa.sync();
-  console.log('Sincronizado');
-}
+//Salvar uma pessoa
+// const pessoa = require('./models/Pessoa');
+// async function salvar(){
+//   const paulo = pessoa.build({
+//     nome: "Paulo",
+//     email: "paulo@gmail.com"
+//   });
+//   await paulo.save();
+// }
+// salvar();
 
-sincronizar();
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const port = process.env.API_PORT
+
+app.get('/pessoas', (req, res) => {
+  res.send('Listar pessoas')
+})
+
+app.get('/pessoas/:id', (req, res) => {
+  res.send('Buscar pessoas')
+})
+
+app.post('/pessoas', (req, res) => {
+  res.send('Salvar pessoas')
+})
+
+app.delete('/pessoas', (req, res) => {
+  res.send('Deletar pessoas')
+})
+
+app.put('/pessoas/:id', (req, res) => {
+  res.send('Atualizar pessoas')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
